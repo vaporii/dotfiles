15f74787827e06cee8879b7fb9e43fddf95603e3
vaporii <phi548182@gmail.com>
2024-06-16 17:29:53 -0500

    small changes and added wallpaper

--- setconfig.sh
+++ setconfig.sh
@@ -6,7 +6,7 @@ echo ""
 
 echo -e "\e[33mfeh (wallpaper):\e[0m \e[4mhttps://archlinux.org/packages/extra/x86_64/feh/\e[0m"
 echo -e "\e[33mdunst (notifications):\e[0m \e[4mhttps://github.com/dunst-project/dunst\e[0m"
-echo -e "\e[33mkitty (terminal):\e[0m \e[4mhttps://sw.kovidgoyal.net/kitty/binary/\e[0m"
+echo -e "\e[33mkitty (terminal):\e[0m \e[4mhttps://sw.kovidgoyal.net/kitty/binary/\e[0m (make sure kitty is in your PATH!)"
 echo -e "\e[33mpicom (compositor):\e[0m \e[4mhttps://github.com/yshui/picom\e[0m"
 echo -e "\e[33mpolybar (status bar):\e[0m \e[4mhttps://github.com/polybar/polybar\e[0m"
 echo -e "\e[33mrofi (app search):\e[0m \e[4mhttps://github.com/davatorium/rofi\e[0m"
@@ -38,11 +38,12 @@ cp ${dir}/picom -Rf ${dst}/
 cp ${dir}/polybar -Rf ${dst}/
 cp ${dir}/rofi -Rf ${dst}/
 
-cp ${dir}/starship.toml ${dst}/starship.toml
-cp ${dir}/hyfetch.json ${dst}/hyfetch.json
+cp ${dir}/starship.toml ${dst}/
+cp ${dir}/hyfetch.json ${dst}/
+cp ${dir}/wallpaper.jpg ${dst}/
 
 mkdir -p ${dst}/scripts
-cp ${dir}/scripts/media.sh ${dst}/scripts/media.sh
-cp ${dir}/scripts/volume.sh ${dst}/scripts/volume.sh
+cp ${dir}/scripts/media.sh ${dst}/scripts/
+cp ${dir}/scripts/volume.sh ${dst}/scripts/
 
 echo -e "\e[1mInstallation complete, please reload your WM (or just reboot your PC)\e[0m"
